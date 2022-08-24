8f6dc9bcef8e2426a1666e84fe85c92a92016b97
Sushil Jhajhra <sushil.jhajhra@sapient.com>
2022-08-24 19:43:31 +0530

    initial commit

---
+++ wg-user/src/main/java/com/wellgreens/ps/wguser/WgUserApplication.java
@@ -0,0 +1,13 @@
+package com.wellgreens.ps.wguser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgUserApplication.class, args);
+	}
+
+}
---
+++ wg-user/src/test/java/com/wellgreens/ps/wguser/WgUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellgreens.ps.wguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
