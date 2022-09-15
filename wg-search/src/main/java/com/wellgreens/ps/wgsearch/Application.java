a6e1b8d4be61d4a50dad44777e502a2d70dd80a5
Sushil Jhajhra <sushil.jhajhra@publicissapient.com>
2022-09-15 19:17:43 +0530

    updated test cases

--- wg-search/src/main/java/com/wellgreens/ps/wgsearch/Application.java
+++ wg-search/src/main/java/com/wellgreens/ps/wgsearch/Application.java
@@ -2,11 +2,9 @@ package com.wellgreens.ps.wgsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@EnableReactiveElasticsearchRepositories
 @CrossOrigin
 public class Application {
 
--- wg-search/src/test/java/com/wellgreens/ps/wgsearch/SearchServiceTest.java
+++ wg-search/src/test/java/com/wellgreens/ps/wgsearch/SearchServiceTest.java
@@ -1,16 +1,16 @@
 package com.wellgreens.ps.wgsearch;
 
-import com.graphql.spring.boot.test.GraphQLTest;
 import com.wellgreens.ps.wgsearch.domain.entity.Category;
-import com.wellgreens.ps.wgsearch.domain.entity.Item;
-import com.wellgreens.ps.wgsearch.domain.entity.Nutrition;
 import com.wellgreens.ps.wgsearch.domain.service.SearchService;
+import com.wellgreens.ps.wgsearch.domain.service.adapter.vo.Item;
+import com.wellgreens.ps.wgsearch.domain.service.adapter.vo.Nutrition;
 import com.wellgreens.ps.wgsearch.infrastructure.repository.SearchRepository;
+import com.wellgreens.ps.wgsearch.infrastructure.service.SearchDataService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebFlux;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,38 +19,43 @@ import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 
 @WebFluxTest(SearchService.class)
 @ExtendWith(SpringExtension.class)
-//@GraphQLTest
 class SearchServiceTest {
 
     @Autowired
     private SearchService searchService;
 
     @MockBean
-    private SearchRepository searchRepositoryMock;
+    private SearchDataService searchDataService;
 
     @Test
     public void getCategoriesTest() {
 
         Item item = Item.builder()
-                .id(1001L).imageUrl("test").imgAltTxt("test").name("Orange").description("This is Orange!!")
-                .nutritionalInfo(Nutrition.builder().id(1002L).calories("11.3F")
-                        .carbs("9.3F")
-                        .fat("4.3F").protein("2.8F").build())
+                .id(1001L)
+                .imageUrl("test")
+                .imgAltTxt("test")
+                .name("Orange")
+                .description("This is Orange!!")
+                .nutritionalInfo(
+                        Nutrition.builder()
+                                .id(1002L)
+                                .calories("11.3F")
+                                .carbs("9.3F")
+                                .fat("4.3F")
+                                .protein("2.8F")
+                                .build())
                 .build();
 
         Category category = Category.builder().id(1000L).name("FRUITS").items(List.of(item)).build();
 
         Flux<Category> categoryFlux = Flux.just(category);
 
-        when(searchRepositoryMock.findAll()).thenReturn(categoryFlux);
-
-//        doReturn(categoryFlux).when(searchRepositoryMock.findAll());
+        when(searchDataService.getAllCategories()).thenReturn(categoryFlux);
 
         Flux<Category> categoryFluxResponse = searchService.getCategories();
         Assertions.assertNotNull(categoryFluxResponse.blockFirst());
